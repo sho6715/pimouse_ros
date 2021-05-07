ed3161cae8ae06ac674b7d3c66912076d10e0791
Sho Sato <sho.hedwig.s@gmail.com>
2021-05-07 18:41:13 +0900

    add updatewq

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,11 +1,10 @@
 #!/bin/bash -xve
 
 #required packages
-python -m pip install --upgrade pip
-pip install catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
+pip install --user catkin_pkg
+pip install --user empy
+pip install --user pyyaml
+pip install --user rospkg
 
 #ros install
 cd ..
