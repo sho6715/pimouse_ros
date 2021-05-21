edc2d7374e9faf282f8007f198c24781cf2ab631
Sho Sato <sho.hedwig.s@gmail.com>
2021-05-21 20:41:31 +0900

    fix motors2

--- test/travis_test_motors2.py
+++ test/travis_test_motors2.py
@@ -20,9 +20,9 @@ class MotorTest(unittest.TestCase):
 
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
-        self.assertIn('/motors',nodesm"node does not exist")
+        self.assertIn('/motors',nodes"node does not exist")
 
-    def test_put_freq(self):
+    def test_pub_freq(self):
         pub = rospy.Publisher('/motor_raw',MotorFreqs)
         m = MotorFreqs()
         m.left_hz = 123
@@ -53,8 +53,8 @@ class MotorTest(unittest.TestCase):
     def test_on_off(self):
         off = rospy.ServiceProxy('/motor_off', Trigger)
         ret = off()
-        self.assertEqual(ret.success, True, "motor on does not succeeded")
-        self.assertEqual(ret.message, "OFF", "motor on wrong message")
+        self.assertEqual(ret.success, True, "motor off does not succeeded")
+        self.assertEqual(ret.message, "OFF", "motor off wrong message")
         with open("/dev/rtmotoren0", "r") as f:
             data = f.readline()
             self.assertEqual(data,"0\n","wrong value in rtmotor0 at motor off")
@@ -65,7 +65,7 @@ class MotorTest(unittest.TestCase):
         self.assertEqual(ret.message, "ON" ,"motor on wrong message")
         with open("/dev/rtmotoren0","r") as f:
             data = f.readline()
-            self.assertEqual(data."1\n","wrong value in rtmotor0 at motor on")
+            self.assertEqual(data,"1\n","wrong value in rtmotor0 at motor on")
 
 if __name__ == '__main__':
     rospy.init_node('travis_test_motors')
