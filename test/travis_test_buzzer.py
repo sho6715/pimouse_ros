3000e2775bbef3726d9d2f041eb8535733b9366b
Sho Sato <sho.hedwig.s@gmail.com>
2021-06-22 19:01:37 +0900

    motor2 buzzer

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -35,8 +35,7 @@ class BuzzerTest(unittest.TestCase):
         self.client.wait_for_result()
 
         self.assertTrue(self.client.get_result(),"invalid result")
-        self.assertEqual(goal.freqs,self.device_values,"invalid feedback:"
-                + ",".join([str(e) for e in self.device_values]))
+        self.assertEqual(goal.freqs,self.device_values,"invalid feedback:" + ",".join([str(e) for e in self.device_values]))
 
         ###preemption###
         self.device_values = []
