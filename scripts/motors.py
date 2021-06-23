08828f21a69dfecfba121dc0acb08b44fc2411b4
Sho Sato <sho.hedwig.s@gmail.com>
2021-06-23 18:19:48 +0900

    motors.py

--- scripts/motors.py
+++ scripts/motors.py
@@ -70,8 +70,8 @@ class Motor():
         dev = "/dev/rtmotor0"
         try:
             with open(dev,'w') as f:
-                f.write("%d %d %d \n" %
-                    (message.left_hz, message.right_hz,message.duration_ms))
+                f.write("%d %d %d\n" %
+                    (message.left_hz,message.right_hz,message.duration_ms))
 
         except:
             rospy.logerr("cannot write to " + dev)
